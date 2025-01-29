529e192cb82e3c7ad9710e4a9ab0a612046599f4
Brian Power <70545374+brianjpower@users.noreply.github.com>
2025-01-29 10:20:59 +0000

    Updating button locations in addButtons

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ import mysql.connector
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 def addButtons(window):
-    #Need buttons to insert, update, fetch, add, delete or reset
+    #Need buttons to insert, update, fetch, delete and reset
     insertBtn = Button(window,text="Insert", font=("Sans", 12), bg="white")
     insertBtn.place(x=20, y=160)
     updateBtn = Button(window,text="Update", font=("Sans", 12), bg="white")
-    updateBtn.place(x=120, y=160)
+    updateBtn.place(x=80, y=160)
     fetchBtn = Button(window,text="Fetch", font=("Sans", 12), bg="white")
-    fetchBtn.place(x=220, y=160)
-    addBtn = Button(window,text="Add", font=("Sans", 12), bg="white")
-    addBtn.place(x=320, y=160)
+    fetchBtn.place(x=150, y=160)
     deleteBtn = Button(window,text="Delete", font=("Sans", 12), bg="white")
-    deleteBtn.place(x=420, y=160)
+    deleteBtn.place(x=210, y=160)
     resetBtn = Button(window,text="Reset", font=("Sans", 12), bg="white")
-    resetBtn.place(x=520, y=160)
+    resetBtn.place(x=20, y=210)
 
 def addLabels(window):
     #Need labels for employee ID, employee name and employee dept
@@ -30,6 +28,7 @@ def addLabels(window):
     empDept.place(x=20,y=90)
 
 def addEntry(window):
+    #Need entry fields for empID, empName and empDept
     enterID = Entry(window)
     enterID.place(x=170, y=30)
     enterName = Entry(window)
