f60499a3b1b18dfd2f0715d84bdf1f8061b903f4
Brian Power <70545374+brianjpower@users.noreply.github.com>
2025-01-29 10:55:55 +0000

    Added insertData sub to complete action of inserting data and insert button is clicked.  Ensured also that we pass the enterID, enterName and enterDept objects into the insertData sub

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This is a sample Python script.
+import os
 from tkinter import *
+from tkinter import messagebox
 from PIL import Image, ImageTk
 import mysql.connector
 
@@ -7,7 +9,7 @@ import mysql.connector
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 def addButtons(window):
     #Need buttons to insert, update, fetch, delete and reset
-    insertBtn = Button(window,text="Insert", font=("Sans", 12), bg="white")
+    insertBtn = Button(window,text="Insert", font=("Sans", 12), bg="white",command=insertData)
     insertBtn.place(x=20, y=160)
     updateBtn = Button(window,text="Update", font=("Sans", 12), bg="white")
     updateBtn.place(x=80, y=160)
@@ -35,6 +37,8 @@ def addEntry(window):
     enterName.place(x=170, y=60)
     enterDept = Entry(window)
     enterDept.place(x=170, y=90)
+    return enterID, enterName, enterDept  # Return the entry widgets for later use
+
 
 def listBox(window):
     showData = Listbox(window)
@@ -45,12 +49,39 @@ def crud_gui():
     window.geometry("600x270")
     window.title("Employee CRUD App")
     addLabels(window)
-    addEntry(window)
+    enterID, enterName, enterDept = addEntry(window)
     addButtons(window)
     listBox(window)
+    insertData(enterID, enterName, enterDept)
     window.mainloop()
 
-# Press the green button in the gutter to run the script.
+def insertData(enterID, enterName, enterDept):
+    id = enterID.get()
+    name = enterName.get()
+    dept = enterDept.get()
+
+    if id== "" or name== "" or dept== "":
+        messagebox.showwarning("Cannot insert","All three fields, employee id, employee name and employee dept are required")
+    else:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=os.getenv("DB_PASSWORD"),
+            database="employee"
+        )
+        myCur = mydb.cursor()
+        sql = "INSERT INTO employees (empID, empName, empDept) VALUES (id, name, dept)"
+        myCur.execute(sql)
+        mydb.commit()
+
+        #Clear the names filled by the user in the gui so it is ready for next operation
+        enterID.delete(0, END)
+        enterName.delete(0, END)
+        enterDept.delete(0, END)
+        messagebox.showinfo("Insert Status", "Data inserted successfully")
+        myCur.close()
+        mydb.close()
+
 if __name__ == '__main__':
     crud_gui()
 
