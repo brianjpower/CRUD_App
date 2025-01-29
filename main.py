2662e04f0ed08af7f860701c1b29e8a5d1bad715
Brian Power <70545374+brianjpower@users.noreply.github.com>
2025-01-29 10:15:58 +0000

    Adding new sub addButtons to add the required action buttons to the gui

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ import mysql.connector
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def addButtons(window):
+    #Need buttons to insert, update, fetch, add, delete or reset
+    insertBtn = Button(window,text="Insert", font=("Sans", 12), bg="white")
+    insertBtn.place(x=20, y=160)
+    updateBtn = Button(window,text="Update", font=("Sans", 12), bg="white")
+    updateBtn.place(x=120, y=160)
+    fetchBtn = Button(window,text="Fetch", font=("Sans", 12), bg="white")
+    fetchBtn.place(x=220, y=160)
+    addBtn = Button(window,text="Add", font=("Sans", 12), bg="white")
+    addBtn.place(x=320, y=160)
+    deleteBtn = Button(window,text="Delete", font=("Sans", 12), bg="white")
+    deleteBtn.place(x=420, y=160)
+    resetBtn = Button(window,text="Reset", font=("Sans", 12), bg="white")
+    resetBtn.place(x=520, y=160)
+
 def addLabels(window):
     empID = Label(window,text="Employee ID", font=("Serif", 12))
     empID.place(x=20,y=30)
@@ -14,7 +29,6 @@ def addLabels(window):
     empDept.place(x=20,y=90)
     enterID = Entry(window)
     enterID.place(x=170,y=30)
-
 
 def addEntry(window):
     enterID = Entry(window)
@@ -30,8 +44,8 @@ def gui():
     window.title("Employee CRUD App")
     addLabels(window)
     addEntry(window)
+    addButtons(window)
     window.mainloop()
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
