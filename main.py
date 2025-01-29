633bed119d14c3b581023d2dcb91a879a084b47e
Brian Power <70545374+brianjpower@users.noreply.github.com>
2025-01-29 09:54:27 +0000

    Moving gui code from main to gui subroutine and adding window label and window geometry

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import mysql.connector
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def gui():
+    window = Tk()
+    name = Label(text="Employee CRUD App", fg="blue", bg="white", font=("Arial Bold", 12))
+    window.geometry("400x300")
+    window.title("Employee CRUD App")
+    name = Label(text="Employee CRUD App", fg="blue", bg="white", font=("Arial Bold", 12))
+    name.pack()
+    window.mainloop()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    window = Tk()
-    name = Label(text="Employee CRUD App",fg="blue",bg="white",font=("Arial Bold", 12))
-    name.pack()
-    window.mainloop()
+    gui()
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
