67926e308a162af2f4100dab6aade1996c2f089b
Brian Power <70545374+brianjpower@users.noreply.github.com>
2025-01-29 09:49:49 +0000

    Setting up base Tk app window with Application title

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # This is a sample Python script.
+from tkinter import *
+from PIL import Image, ImageTk
+import mysql.connector
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    window = Tk()
+    name = Label(text="Employee CRUD App",fg="blue",bg="white",font=("Arial Bold", 12))
+    name.pack()
+    window.mainloop()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
