d33097d467adcb010a1acdfe2465d5044beee692
Brian Power <70545374+brianjpower@users.noreply.github.com>
2025-02-04 17:45:20 +0000

    Added updateData subroutine

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def addButtons(window,enterID, enterName, enterDept):
     #Need buttons to insert, update, fetch, delete and reset
     insertBtn = Button(window,text="Insert", font=("Sans", 12), bg="white",command=lambda: insertData(enterID, enterName, enterDept))
     insertBtn.place(x=20, y=160)
-    updateBtn = Button(window,text="Update", font=("Sans", 12), bg="white")
+    updateBtn = Button(window,text="Update", font=("Sans", 12), bg="white",command=lambda: updateData(enterID, enterName, enterDept))
     updateBtn.place(x=80, y=160)
     fetchBtn = Button(window,text="Fetch", font=("Sans", 12), bg="white")
     fetchBtn.place(x=150, y=160)
@@ -82,7 +82,31 @@ def insertData(enterID, enterName, enterDept):
         messagebox.showinfo("Insert Status", "Data inserted successfully")
         myCur.close()
         mydb.close()
-
+def updateData(enterID, enterName, enterDept):
+    id = enterID.get()
+    name = enterName.get()
+    dept = enterDept.get()
+    if id == "" or name == "" or dept == "":
+        messagebox.showwarning("Cannot update",
+                               "All three fields, employee id, employee name and employee dept are required to update en employee entry")
+    else:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=os.getenv("DB_PASSWORD"),
+            database="employee"
+        )
+        myCur = mydb.cursor()
+        sql = "UPDATE empDetails SET empName = %s, empDept = %s WHERE empID = %s"
+        myCur.execute(sql, (name, dept, id))
+        mydb.commit()
+        # Clear the names filled by the user in the gui so it is ready for next operation
+        enterID.delete(0, END)
+        enterName.delete(0, END)
+        enterDept.delete(0, END)
+        messagebox.showinfo("Update Status", "Data updated successfully")
+        myCur.close()
+        mydb.close()
 if __name__ == '__main__':
     crud_gui()
 
